16f9993f3b555c434c6b7d0f74f996759b3e9a7b
kelseyduffy <kduffy@osisoft.com>
2021-11-27 13:51:33 -0500

    both tests pass
    both tests pass, but the logic is not idiomatic rust

--- prep_2020_day01/src/lib.rs
+++ prep_2020_day01/src/lib.rs
@@ -1,26 +1,61 @@
-pub fn part1 (contents: &str) -> i64 {
+pub fn part1 () -> i64 {
 
+    let nums = get_puzzle_input();
+
+    for num in &nums {
+        let remainder = 2020 - num;
+
+        if nums.contains(&remainder) {
+            return num * remainder
+        }
+    }
     0
 }
 
 
-pub fn part2 (contents: &str) -> i64 {
+pub fn part2 () -> i64 {
 
+    let nums = get_puzzle_input();
+
+    for num1 in &nums {
+        for num2 in &nums {
+            let remainder = 2020 - num1 - num2;
+
+            if nums.contains(&remainder) {
+                return num1 * num2 * remainder
+            }
+        }
+    }
     0
+}
+
+pub fn get_puzzle_input() -> Vec<i64> {
+
+    let filename = String::from("input.txt");
+
+    let contents = std::fs::read_to_string(filename)
+        .expect("Failed to open file");
+
+    let mut nums: Vec<i64> = Vec::new();
+
+    for line in contents.lines() {
+
+        nums.push(line.parse().unwrap())
+  
+    }
+
+    nums
+
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::fs;
 
     #[test]
     fn part1_works() {
-        let filename = String::from("input.txt");
-        let contents = fs::read_to_string(filename)
-            .expect("Failed to open file");
 	
-        let rust_answer = part1(&contents);
+        let rust_answer = part1();
         let python_answer = 751776;
 
         assert_eq!(rust_answer, python_answer);
@@ -28,11 +63,8 @@ mod tests {
 
     #[test]
     fn part2_works() {
-        let filename = String::from("input.txt");
-        let contents = fs::read_to_string(filename)
-            .expect("Failed to open file");
 
-        let rust_answer = part2(&contents);
+        let rust_answer = part2();
         let python_answer = 42275090;
 
         assert_eq!(rust_answer, python_answer);
