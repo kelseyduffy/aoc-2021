37f2aa29adac23b324ee919612fa82a4046668aa
Kelsey Duffy <kduffy@osisoft.com>
2021-12-01 00:12:22 -0500

    day1 done with python

---
+++ python/01.py
@@ -0,0 +1,26 @@
+nums = []
+
+with open('01.in','r') as f:
+    for x in f.readlines():
+        nums.append(int(x))
+
+## part 1 ##
+
+inc = 0
+
+for i in range(1, len(nums)):
+    if nums[i] > nums[i-1]:
+        inc += 1
+
+print(inc)
+
+
+## part 2 ##
+
+inc = 0
+
+for i in range(3, len(nums)):
+    if nums[i] >  nums[i-3]:
+        inc += 1
+
+print(inc)
