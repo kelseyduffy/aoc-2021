eef1ee2b3910b3f42ab0483ee8fe26330a2d71a5
kelseyduffy <kduffy@osisoft.com>
2021-12-05 23:50:16 -0500

    unit test overflows the stack
    i might want to fix that at some point...

--- day05/src/lib.rs
+++ day05/src/lib.rs
@@ -1,13 +1,22 @@
 use std::fmt::Error;
+use std::cmp;
 
 pub struct Point {
-    x: i32,
-    y: i32,
+    x: u32,
+    y: u32,
 }
 
 pub struct Line {
     start: Point,
     end: Point,
+    style: Direction,
+}
+
+pub enum Direction {
+    Horizontal(i32),
+    Vertical(i32),
+    Diagnol(i32, i32),
+    Unknown,
 }
 
 impl Point {
@@ -23,10 +32,28 @@ impl Point {
 impl Line {
     pub fn new(input: &str) -> Line {
         let parts: Vec<&str> = input.split(" -> ").collect();
-        Line { 
-            start: Point::new(parts[0]), 
-            end: Point::new(parts[1]), 
+        
+        let start = Point::new(parts[0]);
+        let end = Point::new(parts[1]);
+        let mut style = Direction::Unknown;
+
+        if start.x == end.x {
+            style = Direction::Vertical(if start.x < end.x {1} else {-1});
         }
+
+        else if start.y == end.y {
+            style = Direction::Horizontal(if start.y < end.y {1} else {-1});
+        }
+
+        else {
+            style = Direction::Diagnol(if start.x < end.x {1} else {-1}, if start.y < end.y {1} else {-1});
+        }
+        
+        Line { start, end, style }
+    }
+
+    pub fn max_value(&self) -> u32 {
+        cmp::max(cmp::max(self.start.x, self.end.y), cmp::max(self.start.x, self.end.y))
     }
 }
 
@@ -46,7 +73,51 @@ fn get_puzzle_input() -> Result<Vec<Line>, Error> {
 }
 
 fn process_lines(lines: Vec<Line>, use_diags: bool) -> Option<i32> {
-    None
+    
+    const MAX_VALUE: usize = 1000;
+
+    if lines.len() == 0 {
+        return None
+    }
+
+    // let mut max_val = 0;
+    // for line in lines {
+    //    max_val = cmp::max(max_val, Line::max_value( &line));
+    //}    
+
+    let mut counts = [0; (MAX_VALUE + 1) * (MAX_VALUE) + 1];
+
+    for line in lines {
+        match line.style {
+            Direction::Horizontal(mx) => {
+
+                let diff: i32 = (line.start.x - line.end.x).try_into().unwrap();
+                let y_offset: usize = MAX_VALUE * line.start.y as usize;
+                for i in 0..(diff.abs() + 1) {
+                    counts[(i * mx) as usize + line.start.x as usize + y_offset] += 1;
+                }
+
+            },
+            Direction::Vertical(my) => {
+
+                let diff: i32 = (line.start.y - line.end.y).try_into().unwrap();
+                for i in 0..(diff.abs() + 1) {
+                    counts[(((i * my) + line.start.y as i32) * MAX_VALUE as i32) as usize + line.start.x as usize] += 1;
+                }
+            },
+            Direction::Diagnol(mx, my) => {
+                if use_diags {
+
+                }
+            },
+            Direction::Unknown => (),
+        };
+    }
+
+    Some(counts.iter()
+        .filter(|&&x| x >= 2)
+        .count().try_into().unwrap())
+
 }
 
 #[cfg(test)]
