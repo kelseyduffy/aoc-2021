848094ae6937c93cc53cce0e9acc54c7088a4390
Kelsey Duffy <kduffy@osisoft.com>
2021-12-07 08:50:33 -0500

    day 07 python nicer looking

--- python/07.py
+++ python/07.py
@@ -3,18 +3,8 @@ crabs = [int(num) for num in open('python\\07.in').read().strip().split(',')]
 
 ## part 1 ##
 
-min_dist = sum(crabs)
-
-for i in range(max(crabs)):
-    min_dist = min(sum([abs(crab - i) for crab in crabs]), min_dist)
-
-print(min_dist)
+print(min([sum([abs(crab - i) for crab in crabs]) for i in range(max(crabs))]))
 
 ## part 2 ##
 
-min_dist = sum([sum(range(crab + 1)) for crab in crabs])
-
-for i in range(max(crabs)):
-    min_dist = min(sum([sum(range(abs(crab - i) + 1)) for crab in crabs]), min_dist)
-
-print(min_dist)
+print(min([sum([sum(range(abs(crab - i) + 1)) for crab in crabs]) for i in range(max(crabs))]))
