7f177a9003e284e0831a156d575445ee641e450f
Kelsey Duffy <kduffy@osisoft.com>
2021-12-11 14:11:00 -0500

    day 10 both parts working in rust

--- rust/day10/src/lib.rs
+++ rust/day10/src/lib.rs
@@ -18,21 +18,95 @@ pub fn get_scores() -> (u64, u64) {
 
 pub fn score_input(lines: Vec<Vec<char>>) -> (u64, u64) {
     let mut corrupted_scores = 0;
-    let mut incomplete_scores = 0;
+    let mut incomplete_scores = Vec::new();
 
     for line in lines {
         match score_line(line) {
             Some(Fault::Corrupt(x)) => corrupted_scores += x,
-            Some(Fault::Incomplete(x)) => incomplete_scores += x,
+            Some(Fault::Incomplete(x)) => incomplete_scores.push(x),
             _ => (),
         };
     }
 
-    (corrupted_scores, incomplete_scores)
+    incomplete_scores.sort();
+
+    (corrupted_scores, incomplete_scores[incomplete_scores.len() / 2])
 }
 
-pub fn score_line(_line: Vec<char>) -> Option<Fault> {
-    None
+pub fn score_line(line: Vec<char>) -> Option<Fault> {
+    
+    use std::collections::HashMap;
+
+    if line.len() == 0 {
+        return None
+    }
+
+    let expected_closing_chars = HashMap::from([
+        ('(', ')'),
+        ('[', ']'),
+        ('{', '}'),
+        ('<', '>'),
+    ]);
+
+    let char_scores = HashMap::from([
+        // Incomplete lines
+        ('(', 1),
+        ('[', 2),
+        ('{', 3),
+        ('<', 4),
+        // Corrupted lines
+        (')', 3),
+        (']', 57),
+        ('}', 1197),
+        ('>', 25137),
+    ]);
+
+    let mut current_stack = Vec::new();
+
+    for this_char in line {
+        if expected_closing_chars.contains_key(&this_char) {
+            current_stack.push(this_char);
+        }
+        else {
+            let popped_char = match current_stack.pop() {
+                Some(x) => x,
+                _ => return None,
+            };
+            let expected_char = match expected_closing_chars.get(&popped_char) {
+                Some(x) => x,
+                _ => return None,
+            };
+            if this_char != *expected_char {
+                let score = match char_scores.get(&this_char) {
+                    Some(x) => x,
+                    _ => return None,
+                };
+                return Some(Fault::Corrupt(*score))
+            }
+        }
+    }
+
+    // If it makes it this far, it's not corrupted. Unwind the stack and count the scores
+
+    let mut score = 0;
+
+    let mut stack_empty = false;
+
+    while !stack_empty {
+        match current_stack.pop() {
+            Some(x) => {
+                let this_score = match char_scores.get(&x) {
+                    Some(y) => y,
+                    _ => return None,
+                };
+                score *= 5;
+                score += this_score;
+            },
+            _ => stack_empty = true,
+        };
+    }
+
+    Some(Fault::Incomplete(score))
 }
 
 #[cfg(test)]
