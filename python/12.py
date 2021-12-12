0b04f1e21740defd238b8094f6394a63e00b11fb
kelseyduffy <kduffy@osisoft.com>
2021-12-12 12:23:51 -0500

    day 12 both stars python
    quite possibly the worst code i've ever written

--- python/12.py
+++ python/12.py
@@ -1,29 +1,36 @@
 import copy
 
-def visit_cave(caves, current_cave, route, total_routes):
+def visit_cave(caves, current_cave, route, total_routes, has_small_double):
     
     for cave in caves[current_cave]:
-        
+        this_double_tracker = has_small_double
         entry_route = [x for x in route]
         
         if cave == 'end':
             total_routes += 1
             continue 
+
         elif cave == 'start':
             continue
-        elif cave in entry_route and cave == cave.lower():
-            previous_visits = entry_route.count(cave)
-            if previous_visits >= 2:
+        
+        elif cave == cave.lower():
+            if entry_route.count(cave) > 1:
                 continue
-            
+
+            elif entry_route.count(cave) == 1 and this_double_tracker:
+                continue
+
+            elif entry_route.count(cave) == 1 and not this_double_tracker:
+                this_double_tracker = True    
+
         entry_route.append(cave)
-        total_routes = visit_cave(caves, cave, entry_route, total_routes)
+        total_routes = visit_cave(caves, cave, entry_route, total_routes, this_double_tracker)
     
     return total_routes
 
 routes = []
 
-with open('python/test.in','r') as f:
+with open('python/12.in','r') as f:
     for x in f.readlines():
         routes.append(x.strip())
 
@@ -48,10 +55,11 @@ route = []
 route.append('start')
 
 total_routes = 0
+has_small_double = False
 
 for cave in caves['start']:
     current_route = [x for x in route]
     current_route.append(cave)
-    total_routes = visit_cave(caves, cave, current_route, total_routes)
+    total_routes = visit_cave(caves, cave, current_route, total_routes, has_small_double)
 
 print(total_routes)
