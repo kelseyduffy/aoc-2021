d9b1435f2578d130873699092c0118b1b9071d91
kelseyduffy <kduffy@osisoft.com>
2021-12-14 19:55:39 -0500

    day 7 part 1 rust works

--- rust/day07/src/lib.rs
+++ rust/day07/src/lib.rs
@@ -1,17 +1,17 @@
 use std::fmt::Error;
 
-pub fn part1 () -> Option<u64> {
+pub fn part1 () -> Option<u32> {
     
-    let _crabs = get_puzzle_input().expect("Error obtaining puzzle input");
+    let crabs = get_puzzle_input().expect("Error obtaining puzzle input");
     
-    None
+    count_movements(crabs, false)
 }
 
-pub fn part2 () -> Option<u64> {
+pub fn part2 () -> Option<u32> {
     
-    let _crabs = get_puzzle_input().expect("Error obtaining puzzle input");
+    let crabs = get_puzzle_input().expect("Error obtaining puzzle input");
 
-    None
+    count_movements(crabs, true)
 }
 
 pub fn get_puzzle_input() -> Result<Vec<u32>,Error> {
@@ -20,7 +20,37 @@ pub fn get_puzzle_input() -> Result<Vec<u32>,Error> {
         .split(',')
         .map(|line| line.parse().unwrap())
         .collect())
+}
 
+pub fn count_movements(crabs: Vec<u32>, iterative_sum: bool) -> Option<u32> {
+
+    if crabs.len() == 0 {
+        return None
+    }
+
+    let mut min_fuel = u32::MAX;
+
+    let max_crab = if let Some(x) = crabs.iter().max() {
+        x
+    } else {
+        unreachable!();
+    };
+    let min_crab = if let Some(x) = crabs.iter().min() {
+        x
+    } else {
+        unreachable!();
+    };
+
+    for attack_position in *min_crab as usize..*&((max_crab + 1)) as usize {
+        let fuel = crabs
+            .iter()
+            .map(|crab| {(*crab as i32 - attack_position as i32).abs() as u32})
+            .sum();
+        
+        min_fuel = if fuel < min_fuel {fuel} else {min_fuel};
+    }
+
+    Some(min_fuel)
 }
 
 #[cfg(test)]
@@ -30,7 +60,7 @@ mod tests {
     #[test]
     fn part1_works() {
         let rust_answer = part1();
-        let python_answer: u64 = 356179;
+        let python_answer: u32 = 356179;
 
         match rust_answer {
             Some(x) => assert_eq!(x, python_answer),
@@ -41,7 +71,7 @@ mod tests {
     #[test]
     fn part2_works() {
         let rust_answer = part2();
-        let python_answer: u64 = 99788435;
+        let python_answer: u32 = 99788435;
 
         match rust_answer {
             Some(x) => assert_eq!(x, python_answer),
