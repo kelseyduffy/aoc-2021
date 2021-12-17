835a9a2a94f7ed66f56dbf3024c137dc699591b9
Kelsey Duffy <kduffy@osisoft.com>
2021-12-16 19:27:59 -0500

    some refactoring day 16 python

--- python/16.py
+++ python/16.py
@@ -1,22 +1,23 @@
-def parse_next_packet(i, instruction, version_total):
+def parse_packet(i, instruction, version_total):
 
     version_total += int(instruction[i:i+3], 2)
     i += 3
 
-    type_id = int(instruction[i:i+3], 2)
+    type_id = instruction[i:i+3]
     i += 3
 
-    if type_id == 4:
+    if type_id == '100':
         this_num_str = ''
-
-        keep_going = True
         
-        while(keep_going):
-            keep_going = bool(int(instruction[i]))
+        while(True):
+            keep_going = instruction[i]
             i += 1
 
             this_num_str += instruction[i:i+4]
             i += 4
+
+            if keep_going == '0':
+                break
 
         value = int(this_num_str, 2)
 
@@ -24,7 +25,7 @@ def parse_next_packet(i, instruction, version_total):
         length_type_id = instruction[i]
         i += 1
 
-        these_values = []
+        values = []
 
         if length_type_id == '0':
             # 15 bit representing the number of bits of sub packets contained
@@ -34,11 +35,8 @@ def parse_next_packet(i, instruction, version_total):
             # keep parsing out the next bits until you hit the limit
             stopping_point = i + num_sub_packet_bits
             while(i < stopping_point):
-                i, version_total, this_value = parse_next_packet(i, instruction, version_total)
-                these_values.append(this_value)
-
-            # assert that it didn't go too far
-            assert i == stopping_point
+                i, version_total, this_value = parse_packet(i, instruction, version_total)
+                values.append(this_value)
 
         else:
             # 11 bit representing the number of sub packets immedatiately following
@@ -46,52 +44,46 @@ def parse_next_packet(i, instruction, version_total):
             i += 11
 
             for _ in range(num_sub_packets):
-                i, version_total, this_value = parse_next_packet(i, instruction, version_total)
-                these_values.append(this_value)
+                i, version_total, this_value = parse_packet(i, instruction, version_total)
+                values.append(this_value)
         
-        if type_id == 0:
-            value = sum(these_values)
+        if type_id == '000':
+            value = sum(values)
 
-        elif type_id == 1:
+        elif type_id == '001':
             value = 1
-            for this_value in these_values:
+            for this_value in values:
                 value *= this_value
 
-        elif type_id == 2:
-            value = min(these_values)
+        elif type_id == '010':
+            value = min(values)
 
-        elif type_id == 3:
-            value = max(these_values)
+        elif type_id == '011':
+            value = max(values)
 
-        elif type_id == 5:
-            value = these_values[0] > these_values[1]
+        elif type_id == '101':
+            value = values[0] > values[1]
 
-        elif type_id == 6:
-            value = these_values[0] < these_values[1]
+        elif type_id == '110':
+            value = values[0] < values[1]
             pass
 
-        elif type_id == 7:
-            value = these_values[0] == these_values[1]
+        elif type_id == '111':
+            value = values[0] == values[1]
             pass
     
     return i, version_total, value
 
-instruction = ''
 
 with open('python\\16.in','r') as f:
-    for x in f.readlines()[0].strip():
-        instruction += format(int(x, 16), '#06b')[2:]
-        #instruction += bin(int(f.readlines()[0].strip(), 16))[2:] # doesn't contain leading zeros for the very first character
+    hex_string = f.readlines()[0].strip()
+        
+instruction = bin(int(hex_string, 16))[2:] # doesn't contain leading zeros for the very first character
 
-#print(instruction)
-#print('110100101111111000101000') # test.in
-#print('00111000000000000110111101000101001010010001001000000000') # test2.in
+leading_zeros = '0' * (len(hex_string) * 4 - len(instruction))
+instruction = leading_zeros + instruction
 
-i = 0
-version_total = 0
-
-while(int(instruction[i:], 2) != 0):
-    i, version_total, value = parse_next_packet(i, instruction, version_total)
+i, version_total, value = parse_packet(0, instruction, 0)
     
 ## part 1 ##
 
