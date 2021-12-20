a5fc4b51836b819b174e88e5be9d989ed1238db7
Kelsey Duffy <kduffy@osisoft.com>
2021-12-20 17:11:03 -0500

    day 20 part 1 python done

--- python/20.py
+++ python/20.py
@@ -13,46 +13,37 @@ with open('python/20.in','r') as f:
 print(enhancement)
 print(image)
 
-# add two rings of 0s for padding to never exceed list ranges with -1 or +1 indexes
-for i in range(len(image)):
-    image[i].insert(0,'0') 
-    image[i].insert(0,'0') 
-    image[i].append('0')
-    image[i].append('0')
-image.insert(0,[ch for ch in '0' * len(image[0])])
-image.insert(0,[ch for ch in '0' * len(image[0])])
-image.append([ch for ch in '0' * len(image[0])])
-image.append([ch for ch in '0' * len(image[0])])
+# in the beginning, the void char starts as a 0
+void_char = '0'
 
 ## part 1 ##
 
 # each loop of the image processing
-for counter in range(2):
-    # add two rings of 0 around the existing image, expanding it, since these characters might actually update
-    this_round_void_char = '0'
-    if counter % 2 == 1:
-        this_round_void_char = '1'
+for _ in range(2):
+    # add two rings of the void char around the existing image, expanding it, since these characters might actually update
     for i in range(len(image)):
-        image[i].insert(0,this_round_void_char) 
-        image[i].insert(0,this_round_void_char) 
-        image[i].append(this_round_void_char)
-        image[i].append(this_round_void_char)
-    image.insert(0,[ch for ch in this_round_void_char * len(image[0])])
-    image.insert(0,[ch for ch in this_round_void_char * len(image[0])])
-    image.append([ch for ch in this_round_void_char * len(image[0])])
-    image.append([ch for ch in this_round_void_char * len(image[0])])
-
-    # for each x and y in the expanded image, except for the first outer ring of 0's which are only there for safe lookups
+        image[i].insert(0,void_char) 
+        image[i].insert(0,void_char) 
+        image[i].append(void_char)
+        image[i].append(void_char)
+    image.insert(0,[ch for ch in void_char * len(image[0])])
+    image.insert(0,[ch for ch in void_char * len(image[0])])
+    image.append([ch for ch in void_char * len(image[0])])
+    image.append([ch for ch in void_char * len(image[0])])
+    
     new_image = []
-    for i in range(1,len(image)-1):
-        new_row = [this_round_void_char]
-        for j in range(1, len(image[0])-1):
+    for i in range(len(image)):
+        new_row = []
+        for j in range(len(image[0])):
             binary_string = ''
             for x in range(-1,2):
                 for y in range(-1,2):
                     # concetenate 9 chars into binary string
-                    # wrapping around to -1 is safe because the ends are also 0's
-                    binary_string += image[i+x][y+j]
+                    if 0<=i+x<len(image)-1 and 0<=y+j<len(image[0]):
+                        binary_string += image[i+x][j+y]
+                    else:
+                        # add what the void char was before since this is outside the defined boundaries
+                        binary_string += void_char 
             
             # convert binary string to int
             index = int(binary_string,2)
@@ -61,13 +52,14 @@ for counter in range(2):
             new_row.append(enhancement[index])
         
         # make a new image, putting that character in the x,y spot in the new image
-        new_row.append(this_round_void_char)
         new_image.append(new_row)
 
     # reset for next loop
-    new_image.insert(0,[ch for ch in this_round_void_char * len(new_row)])
-    new_image.append([ch for ch in this_round_void_char * len(new_row)])
     image = new_image
+
+    # find the new void char for the next round
+    binary_void_string = void_char * 9
+    void_char = enhancement[int(binary_void_string,2)]
 
 ones = 0
 for row in image:
