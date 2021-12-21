2e79af5b8e771363fd941db0fc5b698342902b1e
Kelsey Duffy <kduffy@osisoft.com>
2021-12-20 19:19:03 -0500

    day 20 part 2 python completed

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ Rust for learning
 |04|1665|2280|--|17|14200|16017|[![Rust](https://github.com/kelseyduffy/aoc-2021/actions/workflows/rust_day17.yml/badge.svg)](https://github.com/kelseyduffy/aoc-2021/actions/workflows/rust_day17.yml)|
 |05|38916|36070|[![Rust](https://github.com/kelseyduffy/aoc-2021/actions/workflows/rust_day05.yml/badge.svg)](https://github.com/kelseyduffy/aoc-2021/actions/workflows/rust_day05.yml)|18|13493|13295|--|
 |06|__333__|7849|[![Rust](https://github.com/kelseyduffy/aoc-2021/actions/workflows/rust_day06.yml/badge.svg)](https://github.com/kelseyduffy/aoc-2021/actions/workflows/rust_day06.yml)|19|8192|7945|--|
-|07|39518|37296|[![Rust](https://github.com/kelseyduffy/aoc-2021/actions/workflows/rust_day07.yml/badge.svg)](https://github.com/kelseyduffy/aoc-2021/actions/workflows/rust_day07.yml)|20|--|--|--|
+|07|39518|37296|[![Rust](https://github.com/kelseyduffy/aoc-2021/actions/workflows/rust_day07.yml/badge.svg)](https://github.com/kelseyduffy/aoc-2021/actions/workflows/rust_day07.yml)|20|13882|13530|--|
 |08|1878|22657|--|21|--|--|--|
 |09|31083|29188|--|22|--|--|--|
 |10|3306|2974|[![Rust](https://github.com/kelseyduffy/aoc-2021/actions/workflows/rust_day10.yml/badge.svg)](https://github.com/kelseyduffy/aoc-2021/actions/workflows/rust_day10.yml)|23|--|--|--|
--- python/20.py
+++ python/20.py
@@ -1,3 +1,53 @@
+def count_ones(image, enhancement, void_char, loops):
+    for count in range(loops):
+        print(f'starting loop {count}')
+        # add two rings of the void char around the existing image, expanding it, since these characters might actually update
+        for i in range(len(image)):
+            image[i].insert(0,void_char) 
+            image[i].insert(0,void_char) 
+            image[i].append(void_char)
+            image[i].append(void_char)
+        image.insert(0,[ch for ch in void_char * len(image[0])])
+        image.insert(0,[ch for ch in void_char * len(image[0])])
+        image.append([ch for ch in void_char * len(image[0])])
+        image.append([ch for ch in void_char * len(image[0])])
+        
+        new_image = []
+        for i in range(len(image)):
+            new_row = []
+            for j in range(len(image[0])):
+                binary_string = ''
+                for x in range(-1,2):
+                    for y in range(-1,2):
+                        # concetenate 9 chars into binary string
+                        if 0<=i+x<len(image)-1 and 0<=y+j<len(image[0]):
+                            binary_string += image[i+x][j+y]
+                        else:
+                            # add what the void char was before since this is outside the defined boundaries
+                            binary_string += void_char 
+                
+                # convert binary string to int
+                index = int(binary_string,2)
+
+                # look up index in enhancement algorithm
+                new_row.append(enhancement[index])
+            
+            # make a new image, putting that character in the x,y spot in the new image
+            new_image.append(new_row)
+
+        # reset for next loop
+        image = new_image
+
+        # find the new void char for the next round
+        binary_void_string = void_char * 9
+        void_char = enhancement[int(binary_void_string,2)]
+
+    ones = 0
+    for row in image:
+        ones += row.count('1')
+
+    return ones
+
 enhancement = ''
 image = []
 
@@ -10,59 +60,13 @@ with open('python/20.in','r') as f:
         new_line = line.replace('.','0').replace('#','1')
         image.append([ch for ch in new_line.strip()])
 
-print(enhancement)
-print(image)
-
 # in the beginning, the void char starts as a 0
 void_char = '0'
 
 ## part 1 ##
 
-# each loop of the image processing
-for _ in range(2):
-    # add two rings of the void char around the existing image, expanding it, since these characters might actually update
-    for i in range(len(image)):
-        image[i].insert(0,void_char) 
-        image[i].insert(0,void_char) 
-        image[i].append(void_char)
-        image[i].append(void_char)
-    image.insert(0,[ch for ch in void_char * len(image[0])])
-    image.insert(0,[ch for ch in void_char * len(image[0])])
-    image.append([ch for ch in void_char * len(image[0])])
-    image.append([ch for ch in void_char * len(image[0])])
-    
-    new_image = []
-    for i in range(len(image)):
-        new_row = []
-        for j in range(len(image[0])):
-            binary_string = ''
-            for x in range(-1,2):
-                for y in range(-1,2):
-                    # concetenate 9 chars into binary string
-                    if 0<=i+x<len(image)-1 and 0<=y+j<len(image[0]):
-                        binary_string += image[i+x][j+y]
-                    else:
-                        # add what the void char was before since this is outside the defined boundaries
-                        binary_string += void_char 
-            
-            # convert binary string to int
-            index = int(binary_string,2)
+print(count_ones(image, enhancement, '0', 2))
 
-            # look up index in enhancement algorithm
-            new_row.append(enhancement[index])
-        
-        # make a new image, putting that character in the x,y spot in the new image
-        new_image.append(new_row)
+## part 2 ##
 
-    # reset for next loop
-    image = new_image
-
-    # find the new void char for the next round
-    binary_void_string = void_char * 9
-    void_char = enhancement[int(binary_void_string,2)]
-
-ones = 0
-for row in image:
-    ones += row.count('1')
-
-print(ones)
+print(count_ones(image, enhancement, '0', 50))
