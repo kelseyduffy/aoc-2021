2eac317503bc896cb3a2bb5b299bc28bcd64e853
Kelsey Duffy <kduffy@osisoft.com>
2021-12-22 07:21:40 -0500

    day 22 part 2 progress
    i think this framework should be fine, just need to implement the two overlap methods

--- python/22.py
+++ python/22.py
@@ -1,20 +1,26 @@
+def check_overlap(this_cube_region, on_cube_region):
+    return False
+
+def find_new_cubes(this_cube_region, on_cube_region, instruction):
+    new_regions = set()
+
+    return new_regions
+
 rules = []
 
 with open('python\\22.in','r') as f:
     for line in f.readlines():
         rule = line.strip().replace(' ',',').replace('x=','').replace('y=','').replace('z=','').replace('..',',')
         instr,x1,x2,y1,y2,z1,z2 = rule.split(',')
-        rules.append((instr, int(x1), int(x2), int(y1), int(y2), int(z1), int(z2)))
+        rules.append((instr, (int(x1), int(x2), int(y1), int(y2), int(z1), int(z2))))
 
 ## part 1 ##
 
 cubes = {}
 
-#print(lines)
-
 for i,rule in enumerate(rules):
     print(i)
-    instruction, x_low, x_high, y_low, y_high, z_low, z_high = rule
+    instruction, (x_low, x_high, y_low, y_high, z_low, z_high) = rule
 
     for x in range(x_low, x_high+1):
         for y in range(y_low, y_high+1):
@@ -28,3 +34,44 @@ for i,rule in enumerate(rules):
         print(sum(v for _,v in cubes.items()))
         break
 
+## part 2 ##
+
+on_cube_regions = set()
+
+for i, rule in enumerate(rules):
+    print(i)
+    instruction, this_cube_region = rule
+    (x_low, x_high, y_low, y_high, z_low, z_high) = this_cube_region
+
+    cubes_overlapped = False
+    removed_regions = set()
+    new_regions = set()
+
+    for on_cube_region in on_cube_regions:
+
+        # check to see if the new region overlaps with this existing on cube region
+        overlap = check_overlap(this_cube_region, on_cube_region)
+
+        # if it does, mark the existing region for later deletion
+        if overlap:
+            removed_regions.add(on_cube_region)
+
+        # call the overlap function
+        new_regions = find_new_cubes(this_cube_region, on_cube_region, instruction)
+
+        pass
+
+    # delete the marked regions
+    on_cube_regions -= removed_regions
+    on_cube_regions.update(new_regions)
+
+    if not cubes_overlapped and instruction == 'on':
+        on_cube_regions.add(this_cube_region)
+
+# find the final number of on cubes
+count = 0
+for on_cube_region in on_cube_regions:
+    (x_low, x_high, y_low, y_high, z_low, z_high) = on_cube_region
+    count += (x_high - x_low + 1) * (y_high - y_low + 1) * (z_high - z_low + 1)
+
+print(count)
