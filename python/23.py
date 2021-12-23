5f25bcfa770c451f1f9d9a3f0ab0d5d071ceab6f
Kelsey Duffy <kduffy@osisoft.com>
2021-12-23 01:47:30 -0500

    day 23 part 2 algorithm layout notes taken

--- python/23.py
+++ python/23.py
@@ -45,3 +45,38 @@ with open('python\\23.in','r') as f:
 # total is 14415
 
 ## part 2 ##
+
+# wrong: 40521
+
+# track the game state of a tuple of (h1, h2, ... , h11, a1, ... , a4, ... , d4) for what's in each and the score to get there
+    # do i need to track which specific A's, B's, etc have been popped out yet?
+        # i don't think so because you can only pop to the hallway and push to the destination
+        # if it's already in the destination, don't pop it
+        # if it's in the hallway, you can only push to one bucket
+        # if you're in the whole bucket, you can only pop to the hallway
+        # who's been popped yet doesn't need to be tracked
+    # have we been to that state before? with a lower score? if so, abandon this option
+# keep track of 4 stacks -> A stack, B stack, C stack, D stack
+# name each of the A's A1 A2 A3 A4 so that they're treated individually in terms of move out tracking
+# try each "next move" of popping the top one of a stack and putting it onto a random space
+    # try each stack
+    # try each destination space
+    # check for blockers, if blocked, abandon this option
+    # don't try to pop off the top of a stack that is collecting its actual letters at this point
+        # identify it as maybe 'has_bad_letter' or something, or 'has_been_empty'
+# also try each "move in" of any hallway piece
+    # if the destination space is blocked, abandon this route
+    # if the route to the destination is blocked, abandon this route
+# moving directly from starting bucket to destination bucket via clear route should be handled by trying:
+    # move to a hallway spot in between -> if the route is clear, this is guaranteed to exist
+    # move to the bucket
+    # these two cases are already handled above, so special case of going directly to final bucket is not necessary
+# maybe use the priority queue from the path finding day before
+    # insert each move into the queue, with the new score of the current game + the score for having done that move
+    # always look at the lowest score state next
+    # track whether we've been to a specific state before
+    # keep going until it's the turn of the final winning state, that score is the lowest by definition
+# just need to figure out how to detect a stalemate?
+    # detect if the path is blocked by checking each hallway spot it passes through
+    # if it's blocked, that specific pop to that specific hallway spot is abandoned
+    # the whole thing should just take care of itself that way
