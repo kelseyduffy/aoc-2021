83417ac1402f655decd9eafb10131a4e3df1529b
kelseyduffy <kduffy@osisoft.com>
2021-12-24 10:10:48 -0500

    day 24 part 1 python progress
    this approach should finish in around 260 days...

--- python/24.py
+++ python/24.py
@@ -1,62 +1,60 @@
-def next_chunk(current_number, input_state):
+## part 1 ##
+
+# each set of 18 rules is the same structure
+
+# inp w           | w is the digit
+# mul x 0         --| x = 0
+# add x z         --| x = incoming_z
+# mod x 26        --| x = (incoming_z % 26)
+# div z _A_       --| z = incoming_x // _A_
+# add x _B_       --| x = (incoming_z % 26) + _B_
+# eql x w         --| x = 1 if [w == (incoming_z % 26) + _B_] else 0
+# eql x 0         | x = 0 if [w == (incoming_z % 26) + _B_] else 1
+# mul y 0         --| y = 0
+# add y 25        --| y = 25
+# mul y x         --| y = 0 if [w == (incoming_z % 26) + _B_] else 25
+# add y 1         --| y = 1 if [w == (incoming_z % 26) + _B_] else 26
+# mul z y         | z = (incoming_z // _A_) if [w == (incoming_z % 26) + _B_] else 26 * incoming_z
+# mul y 0         --| y = 0
+# add y w         --| y = w
+# add y _C_       --| y = w + _C_
+# mul y x         | y = 0 if [w == (incoming_z % 26) + _B_] else (w + _C_)
+# add z y         | if [w == (incoming_z % 26) + _B_]: z = (incoming_z // _A_) else (26 * incoming_z) + (w + _C_)
+
+# need to track the list of _A_, _B_, and _C_ values
+
+# then recursively solve that equation for z
+# - on each loop passing the next digit for each number
+# - depth of 14
+# - when 14, if z = 0, print number
+
+def check_num(number, incoming_z):
+    # | if [w == (incoming_z % 26) + _B_]: z = (incoming_z // _A_) else (26 * incoming_z) + (w + _C_)
+    global a_sequence, b_sequence, c_sequence, num_sequence
+
+    this_digit = int(number[-1])
+    depth = len(number) - 1
+    z = incoming_z
+
+    if this_digit == (z % 26) + b_sequence[depth]:
+        z = z // a_sequence[depth]
+    else:
+        z = 26 * z + this_digit + c_sequence[depth]
     
-    global lines, checked_states
-
-    depth = len(current_number)
-    
-    if ((input_state['w'],input_state['x'],input_state['y'],input_state['z']),depth, current_number[-1]) in checked_states:
-        return
-    
-    checked_states.add(((input_state['w'],input_state['x'],input_state['y'],input_state['z'], current_number[-1]), depth))
-
-    # process the 18 lines at this depth
-    this_state = {}
-    for k in ['w','x','y','z']:
-        this_state[k] = input_state[k]
-
-    this_state['w'] = int(current_number[-1])
-    
-    start_line = (depth-1) * 18 + 1
-    for raw_line in lines[start_line:start_line + 17]:
-        line = raw_line.split(' ')
-        if line[2] in {'w','x','y','z'}:
-            val2 = this_state[line[2]]
-        else:
-            val2 = int(line[2])
-            
-        
-        if line[0] == 'add':
-            this_state[line[1]] = this_state[line[1]] + val2                    
-
-        elif line[0] == 'mul':
-            this_state[line[1]] = this_state[line[1]] * val2
-
-        elif line[0] == 'div':
-            this_state[line[1]] = this_state[line[1]] // val2
-
-        elif line[0] == 'mod':
-            this_state[line[1]] = this_state[line[1]] % val2
-
-        elif line[0] == 'eql':
-            this_state[line[1]] = 1 if this_state[line[1]] == val2 else 0
-
-    if depth == 14 and this_state['z'] == 0:
-        print(current_number)
+    if depth == 13 and z == 0:
+        print(number)
         exit()
 
-    elif depth == 14:
-        return
-
-    if depth == 7:
-        print(f'7 depth: {current_number}')
+    if depth == 6:
+        print(f'depth of 7: {number}')
     
-    for val in ['9','8','7','6','5','4','3','2','1']:
-        next_chunk(current_number+val,this_state)
+    if depth < 13:
+        for num in num_sequence:
+            check_num(number+num, z)
 
-
+# parse input
 
 lines = []
-
 
 #with open('python/test.in','r') as f:
 with open('python/24.in','r') as f:
@@ -65,26 +63,12 @@ with open('python/24.in','r') as f:
         #lines.append([int(x) for x in line.strip().split(' -> ')])
         lines.append(line.strip())
 
-## part 1 ##
+# specifically grab the sequence of _A_, _B_, and _C_ values:
+a_sequence = [int(line.split(' ')[2]) for line in lines[4::18]]
+b_sequence = [int(line.split(' ')[2]) for line in lines[5::18]]
+c_sequence = [int(line.split(' ')[2]) for line in lines[15::18]]
 
-#inp a - Read an input value and write it to variable a.
-#add a b - Add the value of a to the value of b, then store the result in variable a.
-#mul a b - Multiply the value of a by the value of b, then store the result in variable a.
-#div a b - Divide the value of a by the value of b, truncate the result to an integer, then store the result in variable a. (Here, "truncate" means to round the value toward zero.)
-#mod a b - Divide the value of a by the value of b, then store the remainder in variable a. (This is also called the modulo operation.)
-#eql a b - If the value of a and b are equal, then store the value 1 in variable a. Otherwise, store the value 0 in variable a.
+num_sequence = [str(i) for i in reversed(range(1,10))]
 
-# 14 digit input
-# each digit feeds each of the 14 inputs, 1 at a time
-# if the final z value is 0, it's a valid number
-# find the highest 14 digit number
-
-start_values = {'w':0, 'x':0, 'y':0, 'z':0}
-
-checked_states = set()
-
-current_number = ''
-
-for val in ['9','8','7','6','5','4','3','2','1']:
-    next_chunk(current_number+val,start_values)
-
+for num in num_sequence:
+    check_num(num, 0)
